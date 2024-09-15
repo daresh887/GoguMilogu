7caee704a35362e0b5df2daeb8efb03dd762f26b
Darius Hanganu <daresh.hanganu@gmail.com>
2024-09-15 10:10:37 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import discord
+import matplotlib.pyplot as plt
+import random
+import asyncio
+import imageio
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot is ready as {bot.user}')
+
+def bubble_sort_gif(data, gif_filename='bubble_sort.gif'):
+    images = []
+    n = len(data)
+    
+    fig, ax = plt.subplots()
+
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if data[j] > data[j+1]:
+                data[j], data[j+1] = data[j+1], data[j]
+            
+            ax.clear()
+            ax.bar(range(len(data)), data, color='blue')
+            plt.title(f"Pasul {i * (n - i - 1) + j + 1}")
+            
+            frame_filename = f"frame_{i}_{j}.png"
+            plt.savefig(frame_filename)
+            images.append(imageio.imread(frame_filename))
+            
+            os.remove(frame_filename)
+
+    imageio.mimsave(gif_filename, images, fps=2)
+
+@bot.command()
+async def bubble_sort(ctx):
+    data = [random.randint(1, 100) for _ in range(10)]
+    await ctx.send(f"Sirul: {data}")
+
+    gif_filename = 'bubble_sort.gif'
+    bubble_sort_gif(data, gif_filename)
+
+    await ctx.send(file=discord.File(gif_filename))
+
+    os.remove(gif_filename)
+    
+bot.run(DISCORD_TOKEN)
